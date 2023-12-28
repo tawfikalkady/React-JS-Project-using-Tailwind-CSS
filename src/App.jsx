18ffaa4338833bc5562d4e0999a40839e2f5b7fa
tawfikalkady <tawfikalkady@gmail.com>
2023-12-28 19:02:18 +0200

    completed

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import About from './components/About'
+import Features from './components/Features'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Newsletter from './components/Newsletter'
+import Pricing from './components/Pricing'
+import Footer from './shared/Footer'
+
+function App() {
+
+  return (
+    <>
+    {/* setup font and colors */}
+    <Navbar/>
+    <Home/>
+    <Features/>
+    <About/>
+    <Pricing/>
+    <Newsletter/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
